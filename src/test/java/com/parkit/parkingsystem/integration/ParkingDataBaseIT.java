d396aaa50f11d2090fa04306a78085b771c33b3d
Cadi <grace.kdi@gmail.com>
2021-02-27 10:15:43 +0100

    TODO2 : TestPArkingACar

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -51,7 +51,14 @@ public class ParkingDataBaseIT {
     public void testParkingACar(){
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
-        //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+        //TODO: check that a ticket is actually saved in DB and Parking table is updated with availability
+        Ticket tk = ticketDAO.getTicket("ABCDEF");
+        assertEquals("ABCDEF",tk.getVehicleRegNumber());
+        
+        ParkingSpot ps = tk.getParkingSpot();
+        assertEquals(false,ps.isAvailable());
+      
+        
     }
 
     @Test
